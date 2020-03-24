ccad0a914305157e6d8f3575a6fc2fa299aa8961
dawneraq <dawneraq@gmail.com>
2020-03-24 15:26:44 -0700

    pass users array to UserTable props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserTable from './tables/UserTable';
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Kyle', username: 'DM' },
+    { id: 2, name: 'Kayla', username: 'Ozys' },
+    { id: 3, name: 'Andrew', username: 'Kelzeiros' },
+    { id: 4, name: 'Carissa', username: 'Les Bina' },
+    { id: 5, name: 'Tim', username: 'Hurgus' },
+    { id: 6, name: 'Mallory', username: 'Nayeve' }
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,7 +22,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+          <UserTable users={users} />
         </div>
       </div>
     </div>
